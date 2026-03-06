156e8f7a486cf9040bbc2cf6574d23258b7c77b7
quliyev223 <mikayilquliev@gmail.com>
2026-03-06 11:57:03 +0000

    task

--- task3/back-end/api.py
+++ task3/back-end/api.py
@@ -1,15 +1,14 @@
 from flask import Flask
-from flask_cors import CORS # Import CORS to allow cross-origin requests
+from flask_cors import CORS  # Import CORS to handle cross-origin requests
 
 app = Flask(__name__)
-# Enable CORS for the entire app
-CORS(app)
+CORS(app)  # Enable CORS for all routes so front-end can communicate with back-end
 
+# Define the /api/hello route that returns a simple string
 @app.route('/api/hello')
 def hello_world():
-    # This message will be sent to the front-end
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    # Run the server on port 5252 and make it accessible externally
+    # Run the server on all network interfaces on port 5252
     app.run(host='0.0.0.0', port=5252)
